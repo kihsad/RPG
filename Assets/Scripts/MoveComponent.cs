f82ba5253799105c7fb5a279c95d1458f8ff7f33
whitecloh <118826118+whitecloh@users.noreply.github.com>
2023-06-18 22:26:03 +0300

    idle/walk

--- Assets/Scripts/MoveComponent.cs
+++ Assets/Scripts/MoveComponent.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace RPGPlayer
 {
@@ -8,11 +6,11 @@ namespace RPGPlayer
     {
         [SerializeField]
         private LayerMask _canBeClicked;
-        private NavMeshAgent _agent;
         private Player _player;
         private bool _isMoving = false;
         Animator _animator;
         private Vector3 _targetPosition;
+        private float _distance=0f;
         public bool IsMoving
         {
             get
@@ -27,28 +25,10 @@ namespace RPGPlayer
         }
         private void Start()
         {
-            _agent = GetComponent<NavMeshAgent>();
             _player = GetComponent<Player>();
             _animator = GetComponent<Animator>();
         }
 
-        //public void Moving()
-        //{
-        //    if (Input.GetMouseButtonDown(0))
-        //    {
-        //        Ray agentRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //        RaycastHit hitInfo;
-
-        //        if (Physics.Raycast(agentRay, out hitInfo, 100, _canBeClicked))
-        //        {
-        //            _agent.SetDestination(hitInfo.point);
-        //            IsMoving = true;
-        //            _targetPosition = hitInfo.point;
-        //        }
-        //    }
-        //    //else if (_agent.SetDestination(_targetPosition)== false) IsMoving = false;
-        //    else if (transform.position == _targetPosition) IsMoving = false;
-        //}
         public void Moving()
         {
             if (Input.GetMouseButtonDown(0))
@@ -59,12 +39,20 @@ namespace RPGPlayer
                 if (Physics.Raycast(agentRay, out hitInfo, 100, _canBeClicked))
                 {
                     _player.GetAgent.SetDestination(hitInfo.point);
-                    if (_player.GetAgent.SetDestination(hitInfo.point) == true)
-                        IsMoving = true;
-                    else
-                        IsMoving = false;
                     _targetPosition = hitInfo.point;
                 }
+            }
+        }
+        private void Update()
+        {
+            _distance = Vector3.Distance(_targetPosition, _player.transform.position);
+            if(_distance<=0.5)
+            {
+                IsMoving = false;
+            }
+            else
+            {
+                IsMoving = true;
             }
         }
     }

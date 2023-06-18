dafaa74376f5eafad25f1bb24e6401bce768f243
whitecloh <118826118+whitecloh@users.noreply.github.com>
2023-06-18 21:17:18 +0300

    teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,31 @@
+using RPGPlayer;
+using UnityEngine;
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    private TeleportPosition _tpPos;
+    private Player _player;
+    [SerializeField]
+    private TeleportUI _uiTeleport;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player is null) return;
+        _player.GetComponent<Player>().enabled = false;
+        Debug.Log(name);
+        _uiTeleport.gameObject.SetActive(true);
+    }
+
+    public void Teleportation()
+    {
+        _player.GetAgent.Warp(_tpPos.transform.position);
+        _player.GetComponent<Player>().enabled = true;
+        _uiTeleport.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/TeleportUI.cs
@@ -0,0 +1,18 @@
+using RPGPlayer;
+using UnityEngine;
+
+public class TeleportUI : MonoBehaviour
+{
+    private Player _player;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+        gameObject.SetActive(false);
+    }
+    public void NoTeleport()
+    {
+        _player.GetComponent<Player>().enabled = true;
+        gameObject.SetActive(false);
+    }
+}

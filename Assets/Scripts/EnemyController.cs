bb727b8fa59b37ada7e88fc2c170f2437a290722
whitecloh <118826118+whitecloh@users.noreply.github.com>
2023-06-19 19:02:48 +0300

    Revert "animation"
    This reverts commit 9824f45895d8f7a4304f50f47c2dca896d59f415, reversing
    changes made to 2dfafdb05171f8f482c676436c1fbff246f66359.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,68 +5,30 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField]
-    private LayerMask _layerMask;
-    [SerializeField, Range(0, 100)]
-    private float _distance;
-    [SerializeField]
-    private Transform _detector;
-
-    private float stoppingDistance = 3f;
-
-    public Collider[] _targets = new Collider[10];
-
+    private float timer;
     private Animator _animator;
     private List<Transform> _points = new List<Transform>();
     private NavMeshAgent _agent;
-
-    //private Damager _damager;
-
     void Start()
     {
+        timer = 0;
         _animator = GetComponent<Animator>();
         _agent = GetComponent<NavMeshAgent>();
-        //_damager = GetComponent<Damager>();
+        Transform pointsObject = FindObjectOfType<Point>().transform;
+        foreach (Transform point in pointsObject)
+            _points.Add(point);
+        _agent.SetDestination(_points[0].position);
+    }
 
-    }
-    private void FixedUpdate()
+    
+    void Update()
     {
-        Detect();
+        if (_agent.remainingDistance <= _agent.stoppingDistance)
+            _agent.SetDestination(_points[Random.Range(0, _points.Count)].position);
+        timer += Time.deltaTime;
+        if (timer > 10)
+            _animator.SetBool("isPatrolling", false);
     }
-    public void Detect()
-    {
-        int numColliders = Physics.OverlapSphereNonAlloc(_detector.position, _distance, _targets, _layerMask);
-        if (numColliders == 0) return;
-        float distance;
-        for (int i = 0; i < numColliders; i++)
-        {
-            distance = Vector3.Distance(_agent.transform.position, _targets[i].ClosestPoint(transform.position));
-            _agent.SetDestination(_targets[0].transform.position);
-            if (distance <= stoppingDistance)
-            {
-                _animator.SetBool("isAttacking", true);
-                //_damager.Attack();
 
-            }
-            else
-            {
-                _animator.SetBool("isPatrolling", true);
-                _animator.SetBool("isAttacking", false);
-            }
-            if (distance > _distance)
-            {
-                _distance = 6f;
-            }
-            else
-            {
-                _distance = 15f;
-            }
-        }
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_detector.position, _distance);
-    }
 
 }

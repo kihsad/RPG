89e6cb4befbeae8a581b71eaa59e1e25282283cf
whitecloh <118826118+whitecloh@users.noreply.github.com>
2023-06-20 20:53:18 +0300

    minimap + camera

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,46 @@
+using RPGPlayer;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{    
+	[SerializeField]
+	private Vector3 _offset;
+	[SerializeField]
+	private float _zoomSpeed = 4f;
+	[SerializeField]
+	private float _minZoom = 5f;
+	[SerializeField]
+	private float _maxZoom = 15f;
+	[SerializeField]
+	private float yawSpeed = 100f;
+	private Player _target;
+
+	private float pitch = 1.8f;        
+	private float currentZoom = 5f;
+	private float currentYaw = 0f;
+
+    private void Start()
+    {
+		_target = FindObjectOfType<Player>();
+    }
+    void Update()
+	{
+		
+		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed;
+		currentZoom = Mathf.Clamp(currentZoom, _minZoom, _maxZoom);
+
+		
+		currentYaw += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+	}
+
+	void LateUpdate()
+	{
+		
+		transform.position = _target.transform.position - _offset * currentZoom;
+		
+		transform.LookAt(_target.transform.position + Vector3.up * pitch);
+		
+		transform.RotateAround(_target.transform.position, Vector3.up, currentYaw);
+	}
+
+}
